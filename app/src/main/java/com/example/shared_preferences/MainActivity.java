903d6cecde76ea455e1f745960c0af7cbcc7bb79
ADIR <aa7381@bs.amalnet.k12.il>
2025-02-22 17:06:11 +0200

    1

--- app/src/main/java/com/example/shared_preferences/MainActivity.java
+++ app/src/main/java/com/example/shared_preferences/MainActivity.java
@@ -21,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
     int num2;
     Button count ,reset , exit ;
     EditText random ;
-
     String name,num ;
     TextView counter ;
 
-
-
     @Override
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -37,20 +37,31 @@ public class MainActivity extends AppCompatActivity {
         random = findViewById(R.id.random);
         counter = findViewById(R.id.counter);
 
-
         SharedPreferences settings = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
         name  = settings.getString("thename", "");
         random.setText(name);
         count2 = settings.getInt("thecount",0);
         counter.setText(String.valueOf(count2));
-
-
     }
-    public boolean onCreateOptionsMenu(Menu menu) {
+    /**
+     * create the options menu
+     *
+     * @param menu The options menu
+     * @return return true
+     */
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    /**
+     * Checks the selection in the options menu
+     *
+     * @param menu The selected menu item.
+     * @return return true
+     */
     public boolean onOptionsItemSelected(MenuItem menu)
     {
         String num1 = menu.getTitle().toString();
@@ -58,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
         {
             if(num2 ==1 )
             {
-
             }
             else
                 finish();
@@ -68,22 +78,39 @@ public class MainActivity extends AppCompatActivity {
             Intent si = new Intent(this, Page2.class);
             startActivity(si);
         }
-
         return super.onOptionsItemSelected(menu);
-
     }
 
+
+    /**
+     * count the counter Button and updates the textview
+     *
+     * @param view that collect the clicked .
+     */
     public void tocount(View view)
     {
         count2 ++  ;
         counter.setText(String.valueOf(count2));
     }
+
+    /**
+     * reset the counter  and updates the textview
+     *
+     * @param view  is collect the clicked .
+     */
     public void reset(View view)
     {
         count2 = 0 ;
         counter.setText(String.valueOf(count2));
-
     }
+
+
+    /**
+     * Saves the current counter value and input text to SharedPreferences and exits the activity.
+     *
+     * @param view is collect the clicked.
+     */
+
     public void exit222(View view)
     {
         SharedPreferences settings = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
